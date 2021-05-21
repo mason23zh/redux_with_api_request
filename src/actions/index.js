e84db9cb254a3a134016e6b71de40ba36d2f3faf
Zhengyang Zhao <mason.zh@gmail.com>
2021-05-20 23:24:38 -0500

    add postReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("/post");
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
+export const fetchPosts = () => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
   };
 };
+
+//export const fetchPosts = () => async dispatch => {...}
