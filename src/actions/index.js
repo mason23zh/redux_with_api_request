dc0ab18f36e918748785d0892b3f615614fe7d83
Zhengyang Zhao <mason.zh@gmail.com>
2021-05-21 20:39:42 -0500

    memozie on fetch user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 //export const fetchPosts = () => async dispatch => {...}
 export const fetchPosts = () => {
@@ -8,9 +9,15 @@ export const fetchPosts = () => {
   };
 };
 
+//export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 export const fetchUser = (id) => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: "FETCH_USER", payload: response.data });
+  return (dispatch) => {
+    _fetchUser(id, dispatch);
   };
 };
+
+//memoize only call once
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
